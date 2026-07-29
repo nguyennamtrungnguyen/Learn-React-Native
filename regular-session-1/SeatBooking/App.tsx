c1951a8b15b665d4a8772279863b576af895283c
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-07-29 23:11:22 +0700

    update code seatbooking

--- regular-session-1/SeatBooking/App.tsx
+++ regular-session-1/SeatBooking/App.tsx
@@ -1,6 +1,5 @@
-import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Alert, FlatList, StyleSheet, View } from "react-native";
+import { Alert, Button, FlatList, StyleSheet, View } from "react-native";
 import SeatCard from "./components/SeatCard";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -29,6 +28,46 @@ export default function App() {
       Alert.alert("Thông báo", "Ghế này đã có người đặt");
       return;
     }
+
+    setSeats((prev) =>
+      prev.map((item) => {
+        if (item.seatNumber === seatNumber) {
+          return {
+            ...item,
+            isSelected: !item.isSelected,
+          };
+        }
+        return item;
+      }),
+    );
+  };
+
+  const onSubmit = () => {
+    setSeats((prev) =>
+      prev.map((item) => {
+        if (item.isSelected) {
+          return {
+            ...item,
+            isSelected: false,
+            isBooked: true,
+          };
+        }
+
+        return item;
+      }),
+    );
+  };
+
+  const handleConfirm = () => {
+    Alert.alert("Xác nhận", "Xác nhận ghế đã đặt!", [
+      {
+        text: "Xác nhận",
+        onPress: () => onSubmit(),
+      },
+      {
+        text: "Hủy bỏ",
+      },
+    ]);
   };
   return (
     <SafeAreaView style={styles.container}>
@@ -40,6 +79,10 @@ export default function App() {
           <SeatCard onClick={handleSelect} seat={item}></SeatCard>
         )}
       ></FlatList>
+
+      <View style={styles.buttonBook}>
+        <Button title="Đặt Ghế" onPress={() => handleConfirm()}></Button>
+      </View>
     </SafeAreaView>
   );
 }
@@ -48,5 +91,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+
+  buttonBook: {
+    padding: 10,
   },
 });
