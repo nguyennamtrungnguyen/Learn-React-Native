0056170dcee3fcc655d742f9317c401671c9d848
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-07-31 18:08:08 +0700

    commit lab 03

--- Practice-1/src/index.ts
+++ Practice-1/src/index.ts
@@ -1,6 +1,7 @@
 // Import
 import Person = require("./lab01/Person");
 import { Student } from "./lab02/Student";
+import { Car } from "./lab03/Car";
 
 // --- Lap 01 ---
 // 1. Create a class Person with attributes name and age. Write a method to display this information.
@@ -25,3 +26,8 @@ student.display();
 
 // --- Lap 03 ---
 // 3. Create a class Car with properties brand, model, year. Write a method to show car info.
+
+console.log();
+console.log("--- Test Lab 03 ---");
+const car = new Car("Mercedes", "Maybach SL 680", 2024);
+car.ShowCarInfo();
