701cda7d61b30d9c63d552bc0304205a821bdd02
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-07-31 17:43:25 +0700

    commiit code lab 02

--- Practice-1/src/index.ts
+++ Practice-1/src/index.ts
@@ -1,4 +1,4 @@
-// ==== Lap 01
+// --- Lap 01 ---
 // 1. Create a class Person with attributes name and age. Write a method to display this information.
 
 class Person {
@@ -12,6 +12,33 @@ class Person {
     console.log(`Age: ${this.age}`);
   }
 }
-
-const person = new Person("Nguyen", 20);
+console.log("--- Test Lap 01 ---");
+const person = new Person("Nguyen Nam Trung Nguyen", 21);
 person.display();
+
+// ----------------------------------------------------------------------------- //
+
+// --- Lap 02 ---
+// 2. Write a class Student extending Person with an additional attribute grade. Add a method to
+
+class Student extends Person {
+  constructor(
+    name: string,
+    age: number,
+    public grade: string,
+  ) {
+    super(name, age);
+  }
+
+  display(): void {
+    super.display();
+    console.log(`Grade: ${this.grade}`);
+  }
+}
+console.log();
+
+console.log("--- Test Lab 02 ---");
+const student = new Student("Nguyen Nam Trung Nguyen", 21, "A");
+student.display();
+
+// ----------------------------------------------------------------------------- //
