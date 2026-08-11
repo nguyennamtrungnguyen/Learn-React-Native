68f015cdb38d1ed1385f6a5fd5d29cc88cb900f6
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-08-11 22:13:05 +0700

    commit code lab 13 typescript

--- Exercises-TypeScript-OOP/src/index.ts
+++ Exercises-TypeScript-OOP/src/index.ts
@@ -360,6 +360,37 @@ fish.swim();
 
 // 13. Create an abstract class Shape with method area(). Implement Square and Circle.
 
+abstract class Shape {
+  abstract area(): number;
+}
+
+class Square extends Shape {
+  constructor(private side: number) {
+    super();
+  }
+
+  area(): number {
+    return this.side * this.side;
+  }
+}
+
+class Circle extends Shape {
+  constructor(private side: number) {
+    super();
+  }
+
+  area(): number {
+    return Math.PI * this.side * this.side;
+  }
+}
+console.log();
+console.log("--- Test Lap 13 ---");
+const square = new Square(29);
+console.log("Square area: ", square.area());
+
+const circle = new Circle(20);
+console.log("Circle area: ", circle.area());
+
 // ----------------------------------------------------------------------------- //
 // ----------------------------------------------------------------------------- //
 
