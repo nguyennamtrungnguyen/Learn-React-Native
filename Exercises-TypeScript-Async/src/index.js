f989e3df11ab767d2280c4db3d54e20809d5b8ca
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-08-16 16:01:58 +0700

    chore: remove node_modules from git

--- Exercises-TypeScript-Async/src/index.js
+++ Exercises-TypeScript-Async/src/index.js
@@ -1,6 +1,6 @@
-//      ===== A. Basics with Promise =====
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // 1.  Create a Promise  that returns  the string  "Hello Async" after 2 seconds.
 const lab1 = new Promise((res) => setTimeout(() => res("Hello Async"), 2000));
 lab1.then((msg) => console.log(`Lab1: ${msg}`));
-// -----------------------------------------------------------------------------
-// -----------------------------------------------------------------------------
+//# sourceMappingURL=index.js.map
