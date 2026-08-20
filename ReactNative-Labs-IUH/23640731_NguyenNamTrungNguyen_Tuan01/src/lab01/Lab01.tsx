cc94d7861d2b22a63b75b72ce5b276a43a24178f
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-08-20 20:53:25 +0700

    first commit the exercise week 1

--- ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan01/src/lab01/Lab01.tsx
+++ ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan01/src/lab01/Lab01.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Text, View } from "react-native";
+import { UserCard } from "./UserCard";
 
 /*  Bài 1: Thẻ thông tin thành viên (UserCard)
         Mục tiêu: Biết định nghĩa kiểu dữ liệu cơ bản cho Props của một Functional Component.
@@ -6,7 +8,12 @@ import React from "react";
  	    Kiến thức cần học: interface, primitive types (string, number, boolean).
 */
 const Lab01 = () => {
-  return <div>Lab01</div>;
+  return (
+    <View>
+      <Text>Bài 1: UserCard</Text>
+      <UserCard name="Nguyen Nam Trung Nguyen" age={22} isAdmin={true} />
+    </View>
+  );
 };
 
 export default Lab01;
--- ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan01/src/lab01/UserCard.tsx
+++ ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan01/src/lab01/UserCard.tsx
@@ -1,5 +1,42 @@
+import { Text, View, StyleSheet } from "react-native";
+
 interface UserCardProps {
   name: string;
   age: number;
   isAdmin: boolean;
 }
+export const UserCard = ({ name, age, isAdmin }: UserCardProps) => {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.name}>Tên: {name}</Text>
+      <Text style={styles.age}>Tuổi: {age}</Text>
+      {isAdmin ? (
+        <Text style={styles.admin}>Quản trị viên</Text>
+      ) : (
+        <Text style={styles.member}>Thành viên</Text>
+      )}
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  card: {
+    padding: 16,
+    marginBottom: 12,
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  age: {
+    marginTop: 6,
+  },
+  admin: {
+    marginTop: 6,
+    fontWeight: "bold",
+  },
+  member: {
+    marginTop: 6,
+  },
+});
