da4ba832ad3506dbd9809f76c95ee9358980f555
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-08-21 20:34:07 +0700

    finish lab 01

--- ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan01/App.tsx
+++ ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan01/App.tsx
@@ -3,14 +3,16 @@ import { ScrollView, StyleSheet, Text, View } from "react-native";
 import Lab01 from "./src/lab01/Lab01";
 import { Lab02 } from "./src/lab02/Lab02";
 import Toast from "react-native-toast-message";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <ScrollView>
-      <Lab01 />
-      <Lab02 />
-      <Toast />
-    </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Lab01 />
+        <Toast />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
