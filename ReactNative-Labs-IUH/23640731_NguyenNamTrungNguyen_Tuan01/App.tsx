c671ab4a0571cba7aa1b5964a7c1b846a25b7f16
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-08-27 20:13:20 +0700

    commit code App.tsx

--- ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan01/App.tsx
+++ ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan01/App.tsx
@@ -1,17 +1,43 @@
-import { ScrollView, StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import Toast from "react-native-toast-message";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 import { Lab02 } from "./src/lab02/Lab02";
 import { Lab03 } from "./src/lab03/Lab03";
+import Lab04 from "./src/lab04/Lab04";
+import Lab05 from "./src/lab05/Lab05";
+import Lab06 from "./src/lab06/Lab06";
+import Lab07 from "./src/lab07/Lab07";
+import Lab08 from "./src/lab08/Lab08";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <ScrollView contentContainerStyle={styles.content}>
+        <ScrollView
+          contentContainerStyle={styles.content}
+          showsVerticalScrollIndicator={true}
+        >
+          {/* Bài 2 */}
           <Lab02 />
+
+          {/* Bài 3 */}
           <Lab03 />
+
+          {/* Bài 4 */}
+          <Lab04 />
+
+          {/* Bài 5 */}
+          <Lab05 />
+
+          {/* Bài 6 */}
+          <Lab06 />
+
+          {/* Bài 7 */}
+          <Lab07 />
+
+          {/* Bài 8 */}
+          <Lab08 />
         </ScrollView>
 
         <Toast />
@@ -28,7 +54,7 @@ const styles = StyleSheet.create({
 
   content: {
     alignItems: "center",
-    justifyContent: "center",
     padding: 20,
+    paddingBottom: 40,
   },
 });
