aa6a6c3e3afef6d739f4ceeb8287c30c6b931d62
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-08-30 23:43:46 +0700

    update code lab09

--- ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan02/App.tsx
+++ ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan02/App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Lab08 } from "./src/lab08/Lab08";
+import Lab09 from "./src/lab09/Lab09";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Lab08 />
+      <Lab09 />
     </View>
   );
 }
