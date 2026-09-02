d9ccca5355a7695ea3bc51f2e8c22d9c86971dab
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-09-02 22:06:04 +0700

    commit code lab 12

--- ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan02/App.tsx
+++ ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan02/App.tsx
@@ -4,6 +4,7 @@ import { Lab08 } from "./src/lab08/Lab08";
 import Lab09 from "./src/lab09/Lab09";
 import Lab10 from "./src/lab10/Lab10";
 import Lab11 from "./src/lab11/Lab11";
+import Lab12 from "./src/lab12/Lab12";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Lab09 />
       <Lab10 />
       <Lab11 />
+      <Lab12 />
     </View>
   );
 }
