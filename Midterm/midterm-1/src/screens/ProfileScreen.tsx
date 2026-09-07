c3decda4bb943b0eafd77a5548769c783af7ff95
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-09-07 21:55:15 +0700

    update code

--- Midterm/midterm-1/src/screens/ProfileScreen.tsx
+++ Midterm/midterm-1/src/screens/ProfileScreen.tsx
@@ -1,21 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
-import { TextInput } from "react-native-paper";
+// import { StyleSheet, Text, View } from "react-native";
+// import { TextInput } from "react-native-paper";
 
-export const ProfileScreen = () => {
-  return (
-    <View style={styles.container}>
-      <TextInput label={"Họ tên"} value="Trung Nguyên" />
-      <TextInput label={"MSSV"} value="23640731" />
-      <TextInput label={"Lớp học"} value="DHKTPM19ATT" />
-    </View>
-  );
-};
+// export const ProfileScreen = () => {
+//   return (
+//     <View style={styles.container}>
+//       <TextInput label={"Họ tên"} value="Trung Nguyên" />
+//       <TextInput label={"MSSV"} value="23640731" />
+//       <TextInput label={"Lớp học"} value="DHKTPM19ATT" />
+//     </View>
+//   );
+// };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    gap: 20,
-    padding:
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     gap: 20,
+//     padding:
+//   },
+// });
