4d67a5d154d5d48c258130125786592c93eaeb24
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-09-16 14:27:17 +0700

    commit code gio 5

---
+++ ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan03/BookStore/src/components/common/BottomTabBar.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const BottomTabBar = () => {
+  return (
+    <View>
+      <Text>BottomTabBar</Text>
+    </View>
+  );
+};
+
+export default BottomTabBar;
+
+const styles = StyleSheet.create({});
