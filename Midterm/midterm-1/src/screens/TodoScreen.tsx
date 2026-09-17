00987532949be351bbe992758e5f73ebaf2f863d
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-09-17 23:02:26 +0700

    commit code midterm 01

--- Midterm/midterm-1/src/screens/TodoScreen.tsx
+++ Midterm/midterm-1/src/screens/TodoScreen.tsx
@@ -16,16 +16,16 @@ export const TodoScreen = () => {
   useEffect(() => handleFetch(), []);
 
   if (isLoading)
-    // return (
-    //   <View>
-    //     <ActivityIndicator size={"large"} animating={true} />
-    //   </View>
-    // );
     return (
-      <View style={styles.container}>
-        <Text>Home</Text>
+      <View>
+        <ActivityIndicator size={"large"} animating={true} />
       </View>
     );
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
