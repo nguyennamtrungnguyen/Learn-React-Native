84c5564dc72c267c0ce9a92d4140db17b1c2f2db
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-09-17 23:02:47 +0700

    commit code midterm 01

--- Midterm/midterm-1/src/interfaces/Todo.ts
+++ Midterm/midterm-1/src/interfaces/Todo.ts
@@ -1,5 +1,5 @@
-export interface Todo {
-  id: string;
-  description: string;
-  isCompleted: boolean;
-}
+// export interface Todo {
+//   id: string;
+//   description: string;
+//   isCompleted: boolean;
+// }
