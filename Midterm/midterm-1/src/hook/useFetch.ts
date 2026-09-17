e689482eebe4b760099e56fb61322d03da901b21
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-09-17 23:03:09 +0700

    commit code midterm 01

--- Midterm/midterm-1/src/hook/useFetch.ts
+++ Midterm/midterm-1/src/hook/useFetch.ts
@@ -1,34 +1,34 @@
-// import { useState } from "react";
+import { useState } from "react";
 
-// export const useFetch = (baseUrl: string) => {
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [error, setError] = useState<string | null>(null);
+export const useFetch = (baseUrl: string) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
-//   const request = async (url: string, options?: RequestInit) => {
-//     setIsLoading(true);
-//     setError(null);
+  const request = async (url: string, options?: RequestInit) => {
+    setIsLoading(true);
+    setError(null);
 
-//     try {
-//       const res = await fetch(baseUrl + url, {
-//         headers: { "Content-Type": "application/json" },
-//         ...options,
-//       });
+    try {
+      const res = await fetch(baseUrl + url, {
+        headers: { "Content-Type": "application/json" },
+        ...options,
+      });
 
-//       if (!res.ok) throw new Error("Lỗi");
-//       return res.json();
-//     } catch (error: any) {
-//       setError(error.message);
-//     } finally {
-//       setIsLoading(false);
-//     }
-//   };
+      if (!res.ok) throw new Error("Lỗi");
+      return res.json();
+    } catch (error: any) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-//   const get = (url: string) => request(url, { method: "GET" });
-//   const post = (url: string, data: any) =>
-//     request(url, { method: "POST", body: JSON.stringify(data) });
-//   const put = (url: string, data: any) =>
-//     request(url, { method: "PUT", body: JSON.stringify(data) });
-//   const del = (url: string) => request(url, { method: "DELETE" });
+  const get = (url: string) => request(url, { method: "GET" });
+  const post = (url: string, data: any) =>
+    request(url, { method: "POST", body: JSON.stringify(data) });
+  const put = (url: string, data: any) =>
+    request(url, { method: "PUT", body: JSON.stringify(data) });
+  const del = (url: string) => request(url, { method: "DELETE" });
 
-//   return { isLoading, error, get, post, put, del };
-// };
+  return { isLoading, error, get, post, put, del };
+};
