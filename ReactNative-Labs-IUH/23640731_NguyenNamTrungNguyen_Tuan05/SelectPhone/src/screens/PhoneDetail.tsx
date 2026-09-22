d758ab05f64da57bdf11cf710f0264f2de3ba661
H51M14\Student <Student@gmail.com>
2026-09-22 14:45:47 +0700

    commit code file phonedetail

--- ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan05/SelectPhone/src/screens/PhoneDetail.tsx
+++ ReactNative-Labs-IUH/23640731_NguyenNamTrungNguyen_Tuan05/SelectPhone/src/screens/PhoneDetail.tsx
@@ -1,17 +1,44 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { RootStackParamList } from '../../App'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import AntDesign from '@expo/vector-icons/AntDesign';
+
 type Props = NativeStackScreenProps<RootStackParamList, "Home">
 const PhoneDetail = ({navigation}: Props) => {
   return (
     <View>
-          <Pressable onPress={()=> navigation.navigate("PhoneSelect")}>
-            <Text>Trang chọn sản phẩm</Text>
-          </Pressable>
-           <Pressable onPress={()=> navigation.navigate("Home")}>
-            <Text>Trang chủ</Text>
-          </Pressable>
+          <View>
+            {/* Image Product */}
+            <Image source={{uri: ""}}></Image>
+          </View>
+
+          <View>
+            <Text>
+              {/* title */}
+            </Text>
+            <View>
+              <View>
+                {/* Icon star */}
+                {[1,2,3,4,5].map(()=> <AntDesign name="star" size={24} color="yellow" />)}
+              </View>
+              <Pressable><Text>(Xem 828 đánh giá)</Text></Pressable>
+            </View>
+            {/* Price */}
+            <View>
+              <Text>Price giảm</Text>
+              <Text>Price gốc</Text>
+            </View>
+
+             <View>
+              <Text>Ở ĐÂU RẺ HƠN HOÀN TIỀN</Text>
+              <AntDesign name="question-circle" size={24} color="black" />
+            </View>
+
+            <Pressable><Text>4 MÀU - CHỌN MÀU</Text><AntDesign name="right" size={24} color="black" /></Pressable>
+          </View>
+
+          <Pressable><Text>CHỌN MUA</Text></Pressable>
         </View>
   )
 }
